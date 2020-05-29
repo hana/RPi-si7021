76619c889efc2b6829acdd660f07fa5fdad710cb
hana <hana.1995@mac.com>
2020-05-30 00:57:11 +0100

    Enabled command line argument to set the output path

--- main.py
+++ main.py
@@ -5,16 +5,23 @@ from board import SCL, SDA
 
 from metro import Metro
 
-
 import json
+import sys.argv
+
 
 i2c = I2C(SCL, SDA)
 sensor = adafruit_si7021.SI7021(i2c)
 
 JSON_Path = "./temp.json"
 
+if len(sys.argv)-1):
+    JSON_Path = sys.argv[1]
+
 
 try:
+
+    print("Writing to", JSON_Path)
+    
     data = {
         "temperature": sensor.temperature,
         "humidity":sensor.relative_humidity
