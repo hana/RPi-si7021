2e31504b6e4622b6524c6a7aaa885a408f1ee93f
BuildTools <unconfigured@null.spigotmc.org>
2020-05-30 11:15:03 -0400

    v1.1

--- main.py
+++ main.py
@@ -3,28 +3,27 @@ import adafruit_si7021
 from busio import I2C
 from board import SCL, SDA
 
-from metro import Metro
-
+import datetime
 import json
-import sys.argv
+import sys
 
-
-i2c = I2C(SCL, SDA)
-sensor = adafruit_si7021.SI7021(i2c)
-
-JSON_Path = "./temp.json"
-
-if len(sys.argv)-1):
-    JSON_Path = sys.argv[1]
-
-
+    
 try:
+    i2c = I2C(SCL, SDA)
+    sensor = adafruit_si7021.SI7021(i2c)
+
+    JSON_Path = "./temp.json"
+
+    if (len(sys.argv)-1) :
+        JSON_Path = sys.argv[1]
 
     print("Writing to", JSON_Path)
-    
+
+
     data = {
         "temperature": sensor.temperature,
-        "humidity":sensor.relative_humidity
+        "humidity":sensor.relative_humidity,
+        "update_at":str(datetime.datetime.now())
     }
 
     with open(JSON_Path, 'w') as f:
@@ -32,3 +31,8 @@ try:
 
 except OSError:
     print("Error occured. Maybe no sensor connected?")
+
+except ValueError:
+    print("Error occured. Maybe no sensor connected?")
+except RuntimeError:
+    pass
